82fffd9882999b1ef01ff16a734c7aa352b12d6a
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2026-07-12 14:56:16 +0200

    test(smoke): rename title test to page should have Rolnopol title

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', { tag: ['@smoke', '@critical'] }, async ({ page }) => {
+test('page should have Rolnopol title', { tag: ['@smoke', '@critical'] }, async ({ page }) => {
 	await page.goto('/');
 
 	await expect(page).toHaveTitle(/Rolnopol/);
